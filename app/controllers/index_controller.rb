8a3c27bcfbf9984b1f91d83b9b3aaf7bbd770d4d
Paweł Kondzior <kondzior.p@gmail.com>
2011-04-13 13:52:38 +0200

    Remove remotipart plugin

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,5 +1,14 @@
 class IndexController < ApplicationController
   def form
+    respond_to do |format|
+       @file_resource = FileResource.new
+      format.html do
+
+      end
+      format.js do
+
+      end
+    end
   end
 
 end
