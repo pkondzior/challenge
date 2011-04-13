dfdec9555f3902dfc6cd46eada2e477f11e23d74
Paweł Kondzior <kondzior.p@gmail.com>
2011-04-13 10:40:02 +0200

    Generate basic form

---
+++ app/controllers/index_controller.rb
@@ -0,0 +1,5 @@
+class IndexController < ApplicationController
+  def form
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Challange::Application.routes.draw do
+  get "index/form"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
