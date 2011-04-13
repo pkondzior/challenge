8891e3fedcec49c9f3e9fbe649566c279b55b909
Paweł Kondzior <kondzior.p@gmail.com>
2011-04-13 09:38:07 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Challange::Application.config.secret_token = 'e2518424ee46d866fbb73286aeb7dabf44d926ff774edb17de89066f2ba9537486f39112c985024e60bd8deae0d312d961adf32da3447e6ca809c76a033b8ea5'
