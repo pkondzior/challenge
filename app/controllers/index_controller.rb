08bef72365ca7be08ddee2dd1b92a644662f59fb
Paweł Kondzior <kondzior.p@gmail.com>
2011-04-14 10:48:40 +0200

    More Item refactoring

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -4,25 +4,22 @@ class IndexController < ApplicationController
   end
 
   def upload
-
-    if params[:item]
-      @item = Item.new(params[:item])
-      @item.save
+    @item = Item.new(params[:item])
+    @item.upload_only = true
+    if @item.save
       render(:json => @item.path)
     else
       render(:json => {:error_msg => 'Upload form error, try again'}.to_json)
     end
-  rescue
-    render(:json => {:error_msg => 'Application Error'}.to_json)
+#  rescue
+#    render(:json => {:error_msg => 'Application Error'}.to_json)
   end
 
   def show
     if request.post?
       @item = Item.new(params[:item])
-      unless @item.valid?
+      unless @item.save
         render('form')
-      else
-        @item.save
       end
     else
       redirect_to('/form')
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,35 +1,45 @@
 class Item
   extend ActiveModel::Naming
+  extend ActiveModel::Callbacks
   include ActiveModel::AttributeMethods
   include ActiveModel::Serialization
   include ActiveModel::Conversion
   include ActiveModel::Validations
 
-  define_attribute_methods [:title, :path, :file]
-  attr_accessor :title, :path, :file
+  define_attribute_methods [:title, :path, :file, :upload_only]
+  define_model_callbacks :save, :only => :before
+  attr_accessor :title, :path, :file, :upload_only
 
-  validates_presence_of(:title)
-  validates_presence_of(:path, :if => Proc.new { |item| item.file.nil?  })
+  validates_presence_of(:title, :unless => Proc.new { |item| item.upload_only })
+  validates_presence_of(:path, :if => Proc.new { |item| item.file.nil? })
+
+  before_save :make_file_path
 
   def initialize(attributes = {})
     attributes.stringify_keys!
     @title = attributes["title"]
     @path = attributes["path"]
     @file = attributes["file"]
+    @upload_only = attributes["upload_only"]
   end
 
   def persisted?
     false
   end
 
-  def save
-    if self.file && self.path.blank?
+  def make_file_path
+    if self.file
       dir = Rails.root.join('tmp')
       dir.mkpath unless dir.exist?
-      source = self.file.path
       dest = dir.join(self.file.original_filename)
-      FileUtils.cp(source, dest)
+      FileUtils.cp(self.file.path, dest)
       self.path = dest
+    end
+  end
+
+  def save
+    run_callbacks(:save) do
+      self.valid?
     end
   end
 end
